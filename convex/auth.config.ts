0cc6bf5f0029fbefa39bf5da11b29925c4a8b8e6
Tony Kanyamuka <tonykanyamuka@gmail.com>
2024-06-06 16:02:06 +0200

    integrated with clerk and implemented sign-in & sing-up functions

---
+++ convex/auth.config.ts
@@ -0,0 +1,12 @@
+// https://suitable-moccasin-33.clerk.accounts.dev
+
+const authConfig = {
+  providers: [
+    {
+      domain: "https://suitable-moccasin-33.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
+
+export default authConfig;
