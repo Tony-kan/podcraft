9834be99990e3c0925be4bbe7335861fa8886161
Tony Kanyamuka <tonykanyamuka@gmail.com>
2024-06-13 00:45:37 +0200

    created & implemented the discover page & search functionality

--- app/(root)/discover/page.tsx
+++ app/(root)/discover/page.tsx
@@ -1,9 +1,56 @@
+"use client";
+
+import EmptyState from "@/components/empty-state";
+import LoaderSpinner from "@/components/loader-spinner";
+import PodcastCard from "@/components/podcast-card";
+import SearchBar from "@/components/search-bar";
+import { api } from "@/convex/_generated/api";
+import { useQuery } from "convex/react";
 import React from "react";
 
-const Discover = () => {
+const Discover = ({
+  searchParams: { search },
+}: {
+  searchParams: { search: string };
+}) => {
+  const podcastsData = useQuery(api.podcast.getPodcastBySearch, {
+    search: search || "",
+  });
+
   return (
-    <div>
-      <h1 className="text-20 font-bold text-white-1">Discover podcasts</h1>
+    <div className="flex flex-col gap-9">
+      <SearchBar />
+      <div className="flex flex-col gap-9">
+        <h1 className="text-20 font-bold text-white-1">
+          {!search ? "Discover Trending podcasts" : "Search Reaults for : "}
+          {search && (
+            <span className="text-orange-1 font-bold text-24">{search}</span>
+          )}
+        </h1>
+        {podcastsData ? (
+          <>
+            {podcastsData.length > 0 ? (
+              <div className="podcast_grid">
+                {podcastsData?.map(
+                  ({ _id, podcastTitle, podcastDescription, imageUrl }) => (
+                    <PodcastCard
+                      key={_id}
+                      imgUrl={imageUrl as string}
+                      title={podcastTitle}
+                      description={podcastDescription}
+                      podcastId={_id}
+                    />
+                  )
+                )}
+              </div>
+            ) : (
+              <EmptyState title="No results found" />
+            )}
+          </>
+        ) : (
+          <LoaderSpinner />
+        )}
+      </div>
     </div>
   );
 };
